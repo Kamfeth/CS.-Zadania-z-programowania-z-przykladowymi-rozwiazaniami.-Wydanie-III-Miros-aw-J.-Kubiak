c3f99f17e06f823fda22a89ae3461b5611298798
Kamil Paradowski <kamil.paradowski@yahoo.com>
2023-02-23 09:55:45 +0100

    Rozdział 1: Zadanie 1

---
+++ Rozdział 1/Zadanie 1/Zadanie.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Zadanie
+{
+  public static void Main()
+  {
+    Console.Write("Wprowadź pierwszy bok prostokąta: ");
+    double a = Convert.ToDouble(Console.ReadLine());
+    Console.Write("Wprowadź drugi bok prostokąta: ");
+    double b = Convert.ToDouble(Console.ReadLine());
+    double pole = a * b;
+    Console.WriteLine("Pole prostokąta jest równe " + pole);
+  }
+}
