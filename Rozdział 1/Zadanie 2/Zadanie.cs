b2273c2cd74c6180ebf16be30031ce7bb06a5560
Kamil Paradowski <kamil.paradowski@yahoo.com>
2023-02-27 09:02:23 +0100

    Rozdział 1: Zadanie 2

---
+++ Rozdział 1/Zadanie 2/Zadanie.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Zadanie
+{
+  public static void Main()
+  {
+    Console.WriteLine("Oto wartość liczby π, zaokrąglona do pięciu miejsc po przecinku: {0:F5}", Math.PI);
+  }
+}
