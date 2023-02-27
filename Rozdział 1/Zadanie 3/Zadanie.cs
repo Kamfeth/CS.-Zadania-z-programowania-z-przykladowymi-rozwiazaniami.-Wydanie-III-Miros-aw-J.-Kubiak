cefbdd4914a94dd9c185761942e578ec92ed9c13
Kamil Paradowski <kamil.paradowski@yahoo.com>
2023-02-27 13:56:03 +0100

    Rozdział 1: Zadanie 3

---
+++ Rozdział 1/Zadanie 3/Zadanie.cs
@@ -0,0 +1,9 @@
+using System;
+
+class Zadanie
+{
+  public static void Main()
+  {
+    Console.WriteLine("Pierwiastek kwadratowy z liczby π, w zaokrągleniu do dwóch miejsc wynosi: {0:F2}", Math.Sqrt(Math.PI));
+  }
+}
