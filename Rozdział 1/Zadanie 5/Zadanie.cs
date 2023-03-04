c8117a5a5bd6166542e2ef2fa9abb5a94f113651
Kamil Paradowski <kamil.paradowski@yahoo.com>
2023-03-04 19:57:31 +0100

    Rozdział 1: Zadanie 5

---
+++ Rozdział 1/Zadanie 5/Zadanie.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Zadanie
+{
+  public static void Main()
+  {
+    int a = 37;
+    int b = 11;
+    Console.WriteLine("Wynik dzielenia całkowitego bez reszty dla liczb {0} oraz {1} wynosi: {2}", a, b, a / b);
+  }
+}
