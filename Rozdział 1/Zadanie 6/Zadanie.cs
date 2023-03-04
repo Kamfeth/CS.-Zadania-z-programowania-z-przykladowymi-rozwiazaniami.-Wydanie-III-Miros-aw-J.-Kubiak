ddbf72f1c41763e2afa14ca85ea15ff4e98c99bf
Kamil Paradowski <kamil.paradowski@yahoo.com>
2023-03-04 21:08:45 +0100

    Rozdział 1: Zadanie 6

---
+++ Rozdział 1/Zadanie 6/Zadanie.cs
@@ -0,0 +1,11 @@
+using System;
+
+class Zadanie
+{
+  public static void Main()
+  {
+    int a = 37;
+    int b = 11;
+    Console.WriteLine("Reszta z dzielenia całkowitego dwóch liczb całkowitych {0} oraz {1} wynosi: {2}", a, b, a % b);
+  }
+}
